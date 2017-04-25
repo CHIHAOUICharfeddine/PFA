62925c32b8f9125f7bdc7ec27f2aa3af8905170e
Nabil005 <nabiladouni951@gmail.com>
2017-04-25 18:52:35 +0100

    Nouveau
    :pp

--- e_sub/src/pages/login/login.ts
+++ e_sub/src/pages/login/login.ts
@@ -1,17 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {TabsPage} from '../tabs/tabs';
+import { LoadingController } from 'ionic-angular';
+import jQuery from "jquery";
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController) {
-
+  
+  loader:any ;
+  nom:string;
+  err:string;
+  constructor(public navCtrl: NavController , public loadingCtrl: LoadingController) {
+   this.nom="";
+   this.err="";
   }
   mainpages(){
-      this.navCtrl.push(TabsPage);
+     //this.navCtrl.push(TabsPage);
+      
+  }
+  presentLoading() {
+    this.loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    this.loader.present();
+     //this.loader.dismiss();
+    this.navCtrl.push(TabsPage);
+  }
+  
+  onBlurMethod(){
+    if(this.nom.length==0){
+        this.err="Enter Username";
+    }else if(this.nom.length<2){
+        this.err="Username too short";
+    } 
+    
+    else if(this.nom.length>20){
+        this.err="Username too long";
+    }else { this.err="" ;}
+   
   }
 
 }
